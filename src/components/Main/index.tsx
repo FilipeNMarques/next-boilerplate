df6b83dfcc9126fb8a1b49c09ca62a1179b7ac33
Filipe Marques <filipen@rpc.com.br>
2020-08-21 23:02:57 -0300

    feature: Adicionando styled-components e estilo main

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,17 @@
+import * as S from './styles'
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo1.svg"
+      alt="Imagem de uma átomo com React avançado escrito ao lado"
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>ReactJs, NextJs e Styled-components </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Imagem de um desenvolvedor sentado numa cadeira escrevendo um código no computador"
+    />
+  </S.Wrapper>
 )
 
 export default Main
